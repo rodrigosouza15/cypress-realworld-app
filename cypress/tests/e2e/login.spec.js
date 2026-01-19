dd3e279a49a79e6d46c0d4a0f044fae23f1ea388
Rodrigo Souza <rodrigo_souzaa130@hotmail.com>
2026-01-19 20:34:43 -0300

    add failure test

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,5 +1,16 @@
 describe('Login com sucesso', () => {
-  it('Login com um usuário válido', () => {
+  it('Login com usuário válido', () => {
     cy.visit('http://localhost:3000/')
-  });
-});
+    cy.get("[name='username']").type('Heath93')
+    cy.get("[name='password']").type('s3cret')
+    cy.get("[type='submit']").click()
+    cy.get("[data-test='main']").contains('Public')
+  })
+  it('Tentativa de login com usuário inválido', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get("[name='username']").type('timao')
+    cy.get("[name='password']").type('s3cret')
+    cy.get("[type='submit']").click()
+    cy.get("[data-test='signin-error']")
+  })
+})
