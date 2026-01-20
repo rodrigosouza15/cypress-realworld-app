329164293b2608eaa8d91421b6c39a9922e6bf56
Rodrigo Souza <rodrigo_souzaa130@hotmail.com>
2026-01-20 20:07:15 -0300

    update spec

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,16 +1,27 @@
+const { Password } = require("@mui/icons-material")
+
 describe('Login com sucesso', () => {
+
+  const selectorlist = {
+    usernameField: "[name='username']",
+    PasswordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopbar: "[data-test='main']",
+    wrognCredentialAlert: "[data-test='signin-error']"
+  }
+
   it('Login com usuário válido', () => {
     cy.visit('http://localhost:3000/')
-    cy.get("[name='username']").type('Heath93')
-    cy.get("[name='password']").type('s3cret')
-    cy.get("[type='submit']").click()
-    cy.get("[data-test='main']").contains('Public')
+    cy.get(selectorlist.usernameField).type('Heath93')
+    cy.get(selectorlist.PasswordField).type('s3cret')
+    cy.get(selectorlist.loginButton).click()
+    cy.get(selectorlist.sectionTitleTopbar).contains('Public')
   })
-  it('Tentativa de login com usuário inválido', () => {
+  it.only('Tentativa de login com usuário inválido', () => {
     cy.visit('http://localhost:3000/')
-    cy.get("[name='username']").type('timao')
-    cy.get("[name='password']").type('s3cret')
-    cy.get("[type='submit']").click()
-    cy.get("[data-test='signin-error']")
+    cy.get(selectorlist.usernameField).type('timao')
+    cy.get(selectorlist.PasswordField).type('s3cret')
+    cy.get(selectorlist.loginButton).click()
+    cy.get(selectorlist.wrognCredentialAlert)
   })
 })
