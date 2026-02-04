bd19059e9e40e40033de5f4fbe75e77894645e57
Rodrigo Souza <rodrigo_souzaa130@hotmail.com>
2026-02-03 22:32:12 -0300

    Remove estruturas que não agregam ao portfólio

--- backend/types.ts
+++
@@ -1,8 +0,0 @@
-import { User as IUser } from "../src/models/user";
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Express {
-    interface User extends IUser {}
-  }
-}
