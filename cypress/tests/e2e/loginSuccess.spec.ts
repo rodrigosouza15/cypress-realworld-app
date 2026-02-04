02acbfdd80d7c357265fe50e166381c45852c21e
Rodrigo Souza <rodrigo_souzaa130@hotmail.com>
2026-02-03 23:02:29 -0300

    teste de commit

--- cypress/tests/e2e/loginSuccess.spec.ts
+++ cypress/tests/e2e/loginSuccess.spec.ts
@@ -10,5 +10,6 @@ describe('Login com sucesso', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.login.userSuccess.username, userData.login.userSuccess.password)
     homePage.checkHomeGrid()
+    //// teste de commit
   })
 })
